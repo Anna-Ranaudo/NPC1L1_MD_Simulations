e0bc2c4fd4c5199bb94d09540473cf126f5dc6d0
Anna-Ranaudo <anna.ranaudo@unimib.it>
2025-12-03 10:49:19 +0100

    filenames in input_structures made consistent

--- scripts/set-up-simulations/0-build-membrane-and-top-files.sh
+++ scripts/set-up-simulations/0-build-membrane-and-top-files.sh
@@ -13,22 +13,22 @@ perl PDB4amber.pl ../../input_structures/6v3h/bound/6V3H-x-MD.pdb
 # 2. Run packmol-memgen to build the membrane and .top and .crd files (input for MD simulations with Amber)
 cd ../../input_structures/6v3f/bound
 
-packmol-memgen  --pdb 6v3f-x-MD.amber.pdb  --pdb2pqr  --lipids  \
+packmol-memgen  --pdb 6V3F-x-MD.amber.pdb  --pdb2pqr  --lipids  \
 POPC:DOPC:POPE:DOPE:POPS:PSM:SSM:OSM:POPI:POPI45H:CHL1//POPC:DOPC:POPE:DOPE:PSM:SSM:OSM:CHL1 \ 
 ratio 0.16:0.31:0.21:0.68:0.38:0.08:0.08:0.13:0.16:0.05:1.00//0.29:0.57:0.04:0.13:0.28:0.28:0.45:0.98 \
 --ppm --keep --parametrize  --ffwat opc --ffprot ff19SB --fflip lipid21  --keepligs --salt
 # run this command twice, because the first time it stops at pdb2pqr
 # it first generates this file (.pdb format, with the membrane)
-#      bilayer_6v3f-x-MD.amber.pdb
+#      bilayer_6V3F-x-MD.amber.pdb
 # then the --parametrize flag makes tleap run, reading leap.in, with the ff specified
 # and it generates the following files:
-#      bilayer_6v3f-x-MD.amber_lipid.pdb
-#      bilayer_6v3f-x-MD.amber_lipid.crd  # coordinates for MD
-#      bilayer_6v3f-x-MD.amber_lipid.top  # topology, can be renamed to .prmtop 
+#      bilayer_6V3F-x-MD.amber_lipid.pdb
+#      bilayer_6V3F-x-MD.amber_lipid.crd  # coordinates for MD
+#      bilayer_6V3F-x-MD.amber_lipid.top  # topology, can be renamed to .prmtop 
 
 # 2.1 do the same for 6v3h
 cd ../../6v3h/bound
-packmol-memgen  --pdb 6v3h-x-MD.amber.pdb  --pdb2pqr  --lipids  \
+packmol-memgen  --pdb 6V3H-x-MD.amber.pdb  --pdb2pqr  --lipids  \
 POPC:DOPC:POPE:DOPE:POPS:PSM:SSM:OSM:POPI:POPI45H:CHL1//POPC:DOPC:POPE:DOPE:PSM:SSM:OSM:CHL1 \
 ratio 0.16:0.31:0.21:0.68:0.38:0.08:0.08:0.13:0.16:0.05:1.00//0.29:0.57:0.04:0.13:0.28:0.28:0.45:0.98 \
 --ppm --keep --parametrize  --ffwat opc --ffprot ff19SB --fflip lipid21  --keepligs --salt
@@ -36,12 +36,12 @@ ratio 0.16:0.31:0.21:0.68:0.38:0.08:0.08:0.13:0.16:0.05:1.00//0.29:0.57:0.04:0.1
 cd ../../
 
 # 3. For the apo structures, we need to remove the ligands (in order to preserve the membrane we just built)
-# we copy e.g. the bilayer_6v3f-x-MD.amber.pdb to the apo structure directory
-cp 6v3f/bound/bilayer_6v3f-x-MD.amber.pdb 6v3f/apo/
-cp 6v3h/bound/bilayer_6v3h-x-MD.amber.pdb 6v3h/apo/
+# we copy e.g. the bilayer_6V3F-x-MD.amber.pdb to the apo structure directory
+cp 6v3f/bound/bilayer_6V3F-x-MD.amber.pdb 6v3f/apo/
+cp 6v3h/bound/bilayer_6V3H-x-MD.amber.pdb 6v3h/apo/
 # we rename them and we remove the ligands (the ligands are the residues n 1262)
-mv 6v3f/apo/bilayer_6v3f-x-MD.amber.pdb 6v3f/apo/bilayer_6v3f-x-MD-apo.amber.pdb
-mv 6v3h/apo/bilayer_6v3h-x-MD.amber.pdb 6v3h/apo/bilayer_6v3h-x-MD-apo.amber.pdb
+mv 6v3f/apo/bilayer_6V3F-x-MD.amber.pdb 6v3f/apo/bilayer_6V3F-x-MD-apo.amber.pdb
+mv 6v3h/apo/bilayer_6V3H-x-MD.amber.pdb 6v3h/apo/bilayer_6V3H-x-MD-apo.amber.pdb
 # we copy the leap.in files to the apo structure directory and modify them accordingly
 cp 6v3f/bound/leap.in 6v3f/apo/
 cp 6v3h/bound/leap.in 6v3h/apo/
